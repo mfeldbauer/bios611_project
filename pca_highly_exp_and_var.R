59a3199c5e94b99552575512167d6bd764d919f6
mfeldbauer <mifeldbauer@gmail.com>
2021-11-30 10:42:20 -0500

    Created a file to do a pca for the samples using the hightly expressed and variable genes and plot it

---
+++ pca_highly_exp_and_var.R
@@ -0,0 +1,39 @@
+#Take in large expression matrix (highly variable and expressed genes)
+#Perform PCA
+
+library(tidyverse)
+library(ggplot2)
+
+expression_data<-read.csv("derived_data/highly_exp_and_mut_genes_matrix_from_large_set.csv", header=T, sep=",", row.names=1, fill=TRUE)
+expression_data<-na.omit(expression_data)
+
+#Function to mean center the samples
+meanCtr<-function(x){
+  apply(x, 2, function(y) y - mean(y))
+}
+
+expression_data<-meanCtr(expression_data)
+expression_data<-as.data.frame(t(expression_data))
+expression_data<-rownames_to_column(expression_data, "PATIENT_ID")
+
+patient_info<-read.table("source_data/clinical_patient_info.txt", header=TRUE, sep="\t", fill=TRUE) %>% 
+  select(c("PATIENT_ID", "CLAUDIN_SUBTYPE"))
+patient_info<-patient_info[patient_info$PATIENT_ID %in% expression_data$PATIENT_ID,]
+
+expression_data<-merge(patient_info, expression_data, by="PATIENT_ID")
+
+pca<-prcomp(expression_data[3:ncol(expression_data)])
+summary(pca)$importance[,1:9] 
+nine_dims<-pca$x %>% as_tibble() %>% select(PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9)
+
+PCi<-data.frame(pca$x, Subtype=expression_data$CLAUDIN_SUBTYPE)
+pc1_var<-summary(pca)$importance[,1][2]*100
+pc2_var<-summary(pca)$importance[,2][2]*100
+
+pdf("figures/pca_plot_high_exp_and_var.pdf", width=7, height=5)
+ggplot(PCi, aes(x=PC1, y=PC2, col=Subtype)) + geom_point(alpha=0.5) +
+  scale_color_manual(values=c("Basal"="red", "Her2"="hotpink","LumA"="darkblue",
+                              "LumB"="skyblue","Normal"="green", "claudin-low"="darkorange", NC="black")) +
+  xlab(str_c("PC1 (", pc1_var, "%)")) + ylab(str_c("PC2 (", pc2_var, "%)"))
+
+dev.off()
