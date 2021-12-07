3f40f806a163bf7457cd53528f31a98e429da99a
mfeldbauer <mifeldbauer@gmail.com>
2021-12-07 10:11:24 -0500

    Changed the max values in the large set clinical plots bc before it was hardcoded. Also tried to use orca to save the plots but I think it will be too complicated with Docker and such so I might just create a shiny app with those plots instead

--- large_set_clinical_data_plots.R
+++ large_set_clinical_data_plots.R
@@ -54,22 +54,26 @@ labs_survival<-levels(survival_factor)
 survival$survival_group<-cut(survival$OS_MONTHS, pretty(survival$OS_MONTHS, 8), labels=labs_survival)
 survival_wide<-tabyl(survival, survival_group, CLAUDIN_SUBTYPE)
 
+max_count_survival<-110
+
 basal_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Basal, type="bar", name="Basal") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 claudin_low_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$`claudin-low`, type="bar", name="Claudin Low") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 her2_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Her2, type="bar", name="HER2") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 lumA_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$LumA, type="bar", name="Luminal A") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 lumB_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$LumB, type="bar", name="Luminal B") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 normal_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Normal, type="bar", name="Normal") %>%
-  layout(yaxis=list(range=c(0,110)))
+  layout(yaxis=list(range=c(0,max_count_survival)))
 fig_os_months<-subplot(basal_os_months, claudin_low_os_months, her2_os_months, lumA_os_months, lumB_os_months, normal_os_months, nrows=3) %>% 
   layout(title="Overall Survival (Months)")
 
 fig_os_months
+
+#orca(fig_os_months, "figures/large_set_overall_survival.pdf")
 
 ################## death from cancer ##################
 deaths<-clinical_info %>% select("CLAUDIN_SUBTYPE", "VITAL_STATUS")
@@ -78,18 +82,20 @@ other_death<-which(deaths_wide$VITAL_STATUS=="Died of Other Causes")
 deaths_wide<-deaths_wide[-other_death,]
 deaths_wide<-deaths_wide[-1,]
 
+max_count_death<-320
+
 basal_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Basal, type="bar", name="Basal") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 claudin_low_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$`claudin-low`, type="bar", name="Claudin Low") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 her2_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Her2, type="bar", name="HER2") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 lumA_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$LumA, type="bar", name="Luminal A") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 lumb_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$LumB, type="bar", name="Luminal B") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 normal_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Normal, type="bar", name="Normal") %>%
-  layout(yaxis=list(range=c(0,320)))
+  layout(yaxis=list(range=c(0,max_count_death)))
 fig_death<-subplot(basal_death, claudin_low_death, her2_death, lumA_death, lumb_death, normal_death, nrows=3) %>% layout(title="Living status")
 
 fig_death
