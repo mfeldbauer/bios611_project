e90d674028929e90de1cd5782bb9fd97d60f565c
mfeldbauer <mifeldbauer@gmail.com>
2021-12-07 14:54:52 -0500

    Updated the name of the derived highly exp and variable csv file. Also changed from individual static plotly plots to a shiny app for the large set like I had for the original set. This shiny app also includes recurrence status and time to recurrence

--- large_set_clinical_data_plots.R
+++ large_set_clinical_data_plots.R
@@ -1,5 +1,6 @@
-#Make a few plots showing clinical data
+#Make a few plots showing clinical data for the large dataset
 
+library(shiny)
 library(tidyverse)
 library(janitor)
 library(plotly)
@@ -12,7 +13,7 @@ sample_info<-read.table("source_data/clinical_sample_info.txt", header=TRUE, sep
   select("PATIENT_ID", "ER_STATUS", "HER2_STATUS")
 
 clinical_info<-merge(patient_info, sample_info, by="PATIENT_ID")
-expression_data<-read.csv("derived_data/highly_exp_and_mut_genes_matrix_from_large_set.csv", header=T, sep=",", row.names=1)
+expression_data<-read.csv("derived_data/highly_exp_and_var_genes_matrix_from_large_set.csv", header=T, sep=",", row.names=1)
 clinical_info<-clinical_info[clinical_info$PATIENT_ID %in% colnames(expression_data),]
 
 ######################## ages #########################
@@ -23,25 +24,6 @@ labs<-levels(age_factor)
 ages$age_group<-cut(clinical_info$AGE_AT_DIAGNOSIS, 
                     pretty(clinical_info$AGE_AT_DIAGNOSIS, 8), labels=labs)
 ages_wide<-tabyl(ages, age_group, CLAUDIN_SUBTYPE)
-
-max_count_ages<-200
-
-basal<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$Basal, type="bar", name="Basal") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-claudin_low<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$`claudin-low`, type="bar", name="Claudin Low") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-her2<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$Her2, type="bar", name="HER2") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-lumA<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$LumA, type="bar", name="Luminal A") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-lumB<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$LumB, type="bar", name="Luminal B") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-normal<-plot_ly(ages_wide, x=~ages_wide$age_group, y=~ages_wide$Normal, type="bar", name="Normal") %>%
-  layout(yaxis=list(range=c(0,max_count_ages)))
-fig<-subplot(basal, claudin_low, her2, lumA, lumB, normal, nrows=3) %>% layout(title="Age at Diagnosis")
-
-fig
-
 
 ################## survival in months #################
 survival<-clinical_info %>% select("CLAUDIN_SUBTYPE", "OS_MONTHS", "VITAL_STATUS")
@@ -55,27 +37,6 @@ labs_survival<-levels(survival_factor)
 survival$survival_group<-cut(survival$OS_MONTHS, pretty(survival$OS_MONTHS, 8), labels=labs_survival)
 survival_wide<-tabyl(survival, survival_group, CLAUDIN_SUBTYPE)
 
-max_count_survival<-110
-
-basal_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Basal, type="bar", name="Basal") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-claudin_low_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$`claudin-low`, type="bar", name="Claudin Low") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-her2_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Her2, type="bar", name="HER2") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-lumA_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$LumA, type="bar", name="Luminal A") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-lumB_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$LumB, type="bar", name="Luminal B") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-normal_os_months<-plot_ly(survival_wide, x=~survival_wide$survival_group, y=~survival_wide$Normal, type="bar", name="Normal") %>%
-  layout(yaxis=list(range=c(0,max_count_survival)))
-fig_os_months<-subplot(basal_os_months, claudin_low_os_months, her2_os_months, lumA_os_months, lumB_os_months, normal_os_months, nrows=3) %>% 
-  layout(title="Overall Survival (Months)")
-
-fig_os_months
-
-#orca(fig_os_months, "figures/large_set_overall_survival.pdf")
-
 ################## death from cancer ##################
 deaths<-clinical_info %>% select("CLAUDIN_SUBTYPE", "VITAL_STATUS")
 deaths_wide<-tabyl(deaths, VITAL_STATUS, CLAUDIN_SUBTYPE)
@@ -83,22 +44,54 @@ other_death<-which(deaths_wide$VITAL_STATUS=="Died of Other Causes")
 deaths_wide<-deaths_wide[-other_death,]
 deaths_wide<-deaths_wide[-1,]
 
-max_count_death<-320
+#################### relapse status ####################
+relapse<-clinical_info %>% select("CLAUDIN_SUBTYPE", "RFS_STATUS")
+table(relapse$RFS_STATUS)
+relapse_wide<-tabyl(relapse, RFS_STATUS, CLAUDIN_SUBTYPE)
+relapse_wide<-relapse_wide[-1,]
 
-basal_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Basal, type="bar", name="Basal") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-claudin_low_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$`claudin-low`, type="bar", name="Claudin Low") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-her2_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Her2, type="bar", name="HER2") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-lumA_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$LumA, type="bar", name="Luminal A") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-lumb_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$LumB, type="bar", name="Luminal B") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-normal_death<-plot_ly(deaths_wide, x=~deaths_wide$VITAL_STATUS, y=~deaths_wide$Normal, type="bar", name="Normal") %>%
-  layout(yaxis=list(range=c(0,max_count_death)))
-fig_death<-subplot(basal_death, claudin_low_death, her2_death, lumA_death, lumb_death, normal_death, nrows=3) %>% layout(title="Living status")
+#################### relapse time ######################
+relapse_time<-clinical_info %>% select("CLAUDIN_SUBTYPE", "RFS_MONTHS")
+relapse_factor<-cut(relapse_time$RFS_MONTHS, pretty(relapse_time$RFS_MONTHS, 8))
+labs_relapse<-levels(relapse_factor)
 
-fig_death
+relapse_time$relapse_group<-cut(relapse_time$RFS_MONTHS, pretty(relapse_time$RFS_MONTHS, 8), labels=labs_relapse)
+rfs_time_wide<-tabyl(relapse_time, relapse_group, CLAUDIN_SUBTYPE)
+
+####################### plots ##########################
+combined_data<-bind_rows(ages_wide, survival_wide, deaths_wide, relapse_wide, rfs_time_wide)
+subtypes<-c("Basal"="Basal", "Claudin Low"="claudin-low", "Her2-enriched"="Her2", 
+            "Luminal A"="LumA", "Luminal B"="LumB", "Normal"="Normal")
+x_axis_options<-c("Age at Diagnosis" = "age_group", 
+                  "Death Status" = "VITAL_STATUS",
+                  "Survival (months)" = "survival_group",
+                  "Time to recurrence (months)" = "relapse_group",
+                  "Recurrence status" = "RFS_STATUS")
+
+server<-function(input, output){
+  output$plot<-renderPlotly({
+    plt<-plot_ly(combined_data, x=~get(input$select_x), y=~get(input$select_y), 
+                 type="bar") %>% layout(
+                   xaxis=list(title=names(x_axis_options[which(x_axis_options==input$select_x)])),
+                   yaxis=list(title='Number of Patients'),
+                   title=names(subtypes[which(subtypes==input$select_y)])
+                 )
+  })
+}
+
+ui<-shinyUI(fluidPage(
+  titlePanel("Explore Patient Data Based on Breast Cancer Subtype"),
+  sidebarLayout(sidebarPanel(selectizeInput(inputId="select_x",
+                                            label="Choose x-axis data",
+                                            choices=x_axis_options),
+                             selectizeInput(inputId="select_y",
+                                            label="Choose subtype",
+                                            choices=subtypes)),
+                mainPanel(plotlyOutput("plot")))
+))
+
+shinyApp(ui, server,
+         options=list(port=8080, host="0.0.0.0"))
+
 
 
